ba133ddf0d00036b6aeb71ab677c7460e9a70b9a
0nionboi <phoenix@redesigned.tech>
2023-02-06 11:11:35 -0500

    Sorts the output of count

--- atbswp/programs/characterCount.py
+++ atbswp/programs/characterCount.py
@@ -9,4 +9,7 @@ for character in message:
 	count.setdefault(character, 0)
 	count[character] = count[character] + 1
 
+# arrange count in numerical order
+count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+
 pprint.pprint(count)
